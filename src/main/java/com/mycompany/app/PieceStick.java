dcac02162bbaaad641f4ce08879981e03f538ab0
MatiasCostantini <matiascostantini24@gmail.com>
2025-09-10 17:45:55 -0300

    agregado de movimientos de pieceT y pieceStick

--- src/main/java/com/mycompany/app/PieceStick.java
+++ src/main/java/com/mycompany/app/PieceStick.java
@@ -1,8 +1,16 @@
 package com.mycompany.app;
 
 public class PieceStick extends Tetris{
-    
+    private String posiciones[];
+
+
     public PieceStick(){
         super("xoooxoooxoooxooo");
     }
+
+   public void movimientos(){
+        posiciones[0] = "xoooxoooxoooxooo";
+        posiciones[1] = "oxoooxoooxoooxoo";
+   }
+
 }
--- src/main/java/com/mycompany/app/PieceT.java
+++ src/main/java/com/mycompany/app/PieceT.java
@@ -1,8 +1,17 @@
 package com.mycompany.app;
 
 public class PieceT extends Tetris{
+    private String posiciones[];
     
     public PieceT(){
         super("oooooooooxooxxxo");
     }
+
+    public void movimientos(){
+        posiciones[0] = "oooooooooxooxxxo";
+        posiciones[1] = "ooooxoooxxooxooo";
+        posiciones[2] = "ooooxxxooxoooooo";
+        posiciones[3] = "oooooxooxxoooxoo";
+   }
+
 }
