91ffc2fd928185ef5fd3bf19cb584de525e9bc1e
colfredo32 <mortolasantino@gmail.com>
2025-09-16 09:04:47 -0300

    Agregado de rotaciones a PieceDogR y PieceDogL

--- src/main/java/com/mycompany/app/PieceDogR.java
+++ src/main/java/com/mycompany/app/PieceDogR.java
@@ -1,15 +1,35 @@
 package com.mycompany.app;
 
 public class PieceDogR extends Tetris{
-    private String posiciones[];
-    
+    private final String [] posiciones;
+    private int rotacionActual = 0;
+
     public PieceDogR(){
             super("oooooooooxxoxxoo", "DogR");
+
+        posiciones = new string [2];
+
+        posiciones[0]= "oooooooooxxoxxoo";
+        posiciones[1]= "xoooxxoooxoooooo";
     }
 
-    public void movimientos(){
-        posiciones[0]= "oooooooooxxoxxoo";
-        posiciones[1]= "oooooxooxxooxooo";
+    @Override
+    public string getPosicionActual() {
+        return posiciones[rotacionActual];
+    }
+
+    @Override
+    public void rotarIzquierda(){
+        rotacionActual = (rotacionActual -1 + posiciones.length) % posiciones.length;
+    }
+
+    @Override
+    public void rotarDerecha(){
+        rotacionActual = (rotacionActual + 1) %posiciones.length;
+    }
+
+    public int getRotacionActual() {
+        return rotacionActual;
     }
 
 }
