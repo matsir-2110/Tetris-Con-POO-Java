ebeadfeba823ac8911e31c3141622d0e5251c3d4
colfredo32 <mortolasantino@gmail.com>
2025-09-16 13:15:15 -0300

    Agregado Test PieceStick

--- src/test/java/com/mycompany/app/PieceStickTest.java
+++ src/test/java/com/mycompany/app/PieceStickTest.java
@@ -1,5 +1,24 @@
 package com.mycompany.app;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class PieceStickTest {
+
+    @Test
+    
+    public void RotarBienStick(){
+        PieceStick ST = new PieceStick();
+
+        // FORMA INICIAL
+        assertEquals("oxoooxoooxoooxoo", ST.getPosicionActual());
+
+        ST.rotarDerecha();
+
+        assertEquals("xxxxoooooooooooo", ST.getPosicionActual());
+
+        ST.rotarIzquierda();
+        assertEquals("oxoooxoooxoooxoo", ST.getPosicionActual());
+    }
+
     
 }
